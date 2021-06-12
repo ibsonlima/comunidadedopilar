79483d8257c08dd492262b6bd08d40f101422587
EvanilsonSantana <evanilson8674@gmail.com>
2021-06-12 11:50:02 -0300

    Update

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('../database');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -24,6 +25,14 @@ const UserSchema = new mongoose.Schema({
         type: Date,
         default: Date.now,
     },
+});
+
+UserSchema.pre('save', function(next) {
+    const hash =  bcrypt.hash(this.password, 10);
+
+    this.password = hash;
+
+    next();
 });
 
 const User = mongoose.model('User', UserSchema);
